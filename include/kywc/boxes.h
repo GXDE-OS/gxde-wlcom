659e621676d7f9bad218d90b262a34ee96b019fc
zhoulei <zhoulei@kylinos.cn>
2023-05-29 09:41:53 +0800

    util: introduce kywc_box
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

---
+++ include/kywc/boxes.h
@@ -0,0 +1,55 @@
+#ifndef _KYWC_BOXES_H_
+#define _KYWC_BOXES_H_
+
+// clang-format off
+
+#include <stdbool.h>
+
+#define KYWC_API static __attribute__((unused)) inline
+
+enum kywc_edges {
+    KYWC_EDGE_NONE = 0,
+    KYWC_EDGE_TOP = 1 << 0,
+    KYWC_EDGE_BOTTOM = 1 << 1,
+    KYWC_EDGE_LEFT = 1 << 2,
+    KYWC_EDGE_RIGHT = 1 << 3,
+};
+
+/* rect with top-left and size */
+struct kywc_box {
+    int x, y, width, height;
+};
+
+KYWC_API void kywc_box_adjust(struct kywc_box *box, int dx, int dy, int dw, int dh)
+{
+    box->x += dx; box->y += dy; box->width += dw; box->height += dh;
+}
+
+KYWC_API void kywc_box_adjust_ex(struct kywc_box *box, int dx1, int dy1, int dx2, int dy2)
+{
+    box->x += dx1; box->y += dy1; box->width += dx1 + dx2; box->height += dy1 + dy2;
+}
+
+KYWC_API struct kywc_box kywc_box_adjusted(const struct kywc_box *box, int dx, int dy, int dw, int dh)
+{
+    return (struct kywc_box){ box->x + dx, box->y + dy, box->width + dw, box->height + dh };
+}
+
+KYWC_API struct kywc_box kywc_box_adjusted_ex(const struct kywc_box *box, int dx1, int dy1, int dx2, int dy2)
+{
+    return (struct kywc_box){ box->x + dx1, box->y + dy1, box->width + dx1 + dx2, box->height + dy1 + dy2 };
+}
+
+KYWC_API bool kywc_box_contains_point(const struct kywc_box *box, int x, int y)
+{
+    return x >= box->x && x < box->x + box->width && y >= box->y && y < box->y + box->height;
+}
+
+KYWC_API bool kywc_box_equal(const struct kywc_box *a, const struct kywc_box *b)
+{
+    return a->x == b->x && a->y == b->y && a->width == b->width && a->height == b->height;
+}
+
+// clang-format on
+
+#endif /* _KYWC_BOXES_H_ */
