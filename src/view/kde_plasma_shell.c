GXDE-OS/gxde-wlcom

commit 5027a26ae9b0c22eff8b83636b8fac66d9250ed1

view: use view workspace layer
Signed-off-by: zhoulei <zhoulei@kylinos.cn>

src/view/kde_plasma_shell.c
@@ -5,6 +5,7 @@
 
 #include "output.h"
 #include "plasma-shell-protocol.h"
+#include "view/workspace.h"
 #include "view_p.h"
 
 #define PLASMA_SURFACE_VERSION 8
@@ -66,7 +67,7 @@ static void handle_set_position(struct wl_client *client, struct wl_resource *re
 static void kde_plasma_surface_apply_role(struct kde_plasma_surface *surface)
 {
     struct ky_scene_node *node = ky_scene_node_from_tree(surface->view->tree);
-    struct view_layer *layer = view_manager_get_layer(LAYER_NORMAL, true);
+    struct view_layer *layer = workspace_layer(surface->view->workspace, LAYER_NORMAL);
 
     switch (surface->role) {
     case ORG_KDE_PLASMA_SURFACE_ROLE_NORMAL:

src/view/view.c
@@ -690,7 +690,7 @@ static void view_reparent_fullscreen(struct view *view, bool active)
     }
 
     /* restore fullscreen view to workspace */
-    struct view_layer *layer = view_manager_get_layer(view->saved.layer, true);
+    struct view_layer *layer = workspace_layer(view->workspace, view->saved.layer);
     ky_scene_node_reparent(ky_scene_node_from_tree(view->tree), layer->tree);
 }
 

commit f7e9406a960257eefcc0d1e24358a191ff91c916

view/plasma: view workspace may be null
Fixes: 5027a26ae9 ("view: use view workspace layer")

Signed-off-by: zhoulei <zhoulei@kylinos.cn>

src/view/kde_plasma_shell.c
@@ -68,10 +68,11 @@ static void handle_set_position(struct wl_client *client, struct wl_resource *re
 static void kde_plasma_surface_apply_role(struct kde_plasma_surface *surface)
 {
     struct ky_scene_node *node = ky_scene_node_from_tree(surface->view->tree);
-    struct view_layer *layer = workspace_layer(surface->view->workspace, LAYER_NORMAL);
+    struct view_layer *layer = NULL;
 
     switch (surface->role) {
     case ORG_KDE_PLASMA_SURFACE_ROLE_NORMAL:
+        layer = view_manager_get_layer(LAYER_NORMAL, true);
         break;
     case ORG_KDE_PLASMA_SURFACE_ROLE_DESKTOP:
         layer = view_manager_get_layer(LAYER_DESKTOP, false);
@@ -97,6 +98,9 @@ static void kde_plasma_surface_apply_role(struct kde_plasma_surface *surface)
     if (surface->role != ORG_KDE_PLASMA_SURFACE_ROLE_NORMAL) {
         view_set_workspace(surface->view, NULL);
         surface->view->base.activatable = false;
+    } else {
+        view_set_workspace(surface->view, workspace_manager_get_current());
+        surface->view->base.activatable = true;
     }
 
     ky_scene_node_reparent(node, layer->tree);