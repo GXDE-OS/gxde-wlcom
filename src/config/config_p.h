5b8c141f14701cb14cc815d4b323c8c8308e3809
sunzhguy <sunzhigang1@kylinos.cn>
2023-08-23 11:18:23 +0800

    config/kde_nightcolor: add ninghtcolor support
    Signed-off-by: sunzhguy <sunzhigang1@kylinos.cn>

--- src/config/config_p.h
+++ src/config/config_p.h
@@ -51,4 +51,14 @@ kde_global_settings_create(struct config_manager *config_manager)
 }
 #endif
 
+#if HAVE_KDE_NIGHTCOLOR
+bool kde_nightcolor_manager_create(struct config_manager *config_manager);
+#else
+static __attribute__((unused)) inline bool
+kde_nightcolor_manager_create(struct config_manager *config_manager)
+{
+    return false;
+}
+#endif
+
 #endif /* _CONFIG_P_H_ */
