e4d1faf9bb0b771e66d0d645b6efc4609202249c
zhoulei <zhoulei@kylinos.cn>
2023-08-31 16:07:56 +0800

    server: destroy xwayland after wayland clients
    This fixes dnd UAF when server finish.
    Needed if wlroots add e209fe2d05 (Fix memory leak in xwayland.c )
    
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/server.c
+++ src/server.c
@@ -282,9 +282,10 @@ void server_finish(struct server *server)
     queue_destroy(&server->queue);
     wl_event_source_remove(server->dbus);
 
+    wl_display_destroy_clients(server->display);
     /* make sure all xwayland-shells are destroyed */
     xwayland_server_destroy();
-    wl_display_destroy_clients(server->display);
+
     wl_display_destroy(server->display);
 
     /* call all server_destroy listeners */
