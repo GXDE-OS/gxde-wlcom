cd1faddc80d90ff08362c081f7bff0afb916a485
zhoulei <zhoulei@kylinos.cn>
2023-11-15 11:27:55 +0800

    config/settings: monitor icon theme signal
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/config/kde_global_settings.c
+++ src/config/kde_global_settings.c
@@ -5,7 +5,9 @@
 #include "config_p.h"
 #include "theme.h"
 
-enum KylinThemeMode { Light, Dark, Default };
+enum KylinTheme { Light = 0, Dark, Default };
+
+enum KylinIconTheme { IconThemeDefault = 0, IconThemeFashion, IconThemeClassical };
 
 static int slot_theme_change(sd_bus_message *msg, void *userdata, sd_bus_error *ret_error)
 {
@@ -34,6 +36,26 @@ static int slot_font_change(sd_bus_message *msg, void *userdata, sd_bus_error *r
     return 0;
 }
 
+static int slot_icon_theme_change(sd_bus_message *msg, void *userdata, sd_bus_error *ret_error)
+{
+    int32_t theme = IconThemeDefault;
+    sd_bus_message_read_basic(msg, 'i', &theme);
+
+    switch (theme) {
+    case IconThemeDefault:
+        theme_manager_set_icon_theme("ukui-icon-theme-default");
+        break;
+    case IconThemeFashion:
+        theme_manager_set_icon_theme("ukui-icon-theme-fashion");
+        break;
+    case IconThemeClassical:
+        theme_manager_set_icon_theme("ukui-icon-theme-classical");
+        break;
+    }
+
+    return 0;
+}
+
 bool kde_global_settings_create(struct config_manager *config_manager)
 {
     sd_bus_match_signal(config_manager->bus, NULL, NULL, "/KGlobalSettings",
@@ -41,6 +63,10 @@ bool kde_global_settings_create(struct config_manager *config_manager)
 
     sd_bus_match_signal(config_manager->bus, NULL, NULL, "/KGlobalSettings",
                         "org.kde.KGlobalSettings", "slotFontChange", slot_font_change, NULL);
+
+    sd_bus_match_signal(config_manager->bus, NULL, NULL, "/KGlobalSettings",
+                        "org.kde.KGlobalSettings", "slotIconThemeChange", slot_icon_theme_change,
+                        NULL);
 
     return true;
 }
