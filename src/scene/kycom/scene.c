9f8f50cf0d0ce3c985fe52bf9ce8d0b09cb7ef82
zhoulei <zhoulei@kylinos.cn>
2023-11-21 09:02:33 +0800

    scene: ensure damage whole when geometry changed
    The damage_ring->current will be empty when dpms on an output.
    This will cause flicker when dpms off and on an output.
    
    Closes: #275
    
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/scene/kycom/scene.c
+++ src/scene/kycom/scene.c
@@ -1039,6 +1039,7 @@ static void scene_output_update_geometry(struct kywc_scene_output *scene_output)
     int width, height;
     wlr_output_transformed_resolution(scene_output->output, &width, &height);
     wlr_damage_ring_set_bounds(&scene_output->damage_ring, width, height);
+    wlr_damage_ring_add_whole(&scene_output->damage_ring);
     wlr_output_schedule_frame(scene_output->output);
 
     scene_node_output_update(&scene_output->scene->group.node, &scene_output->scene->outputs, NULL);
