GXDE-OS/gxde-wlcom

commit 687ece8c7c3e73f0ab310df4644f948b661bd0fa

view: add current_resize_edges flag
Signed-off-by: zhoulei <zhoulei@kylinos.cn>

src/view/interactive.c
@@ -220,6 +220,9 @@ static void interactive_move_show_snap_box(struct interactive_grab *grab, int cu
 
 static void interactive_grab_destroy(struct interactive_grab *grab)
 {
+    if (grab->view) {
+        grab->view->current_resize_edges = KYWC_EDGE_NONE;
+    }
     wl_list_remove(&grab->view_unmap.link);
     seat_end_pointer_grab(grab->seat, &grab->pointer_grab);
     seat_end_keyboard_grab(grab->seat, &grab->keyboard_grab);
@@ -403,6 +406,7 @@ static void interactive_process_resize(struct interactive_grab *grab, double x, 
     }
 
     interactive_resize_constraints(grab, &pending);
+    grab->view->current_resize_edges = grab->resize_edges;
     kywc_view_resize(kywc_view, &pending);
 }
 

include/view/view.h
@@ -127,6 +127,7 @@ struct view {
 
     bool minimized_when_show_desktop;
     bool show_in_all_workspaces;
+    uint32_t current_resize_edges;
 };
 
 struct view_impl {

src/view/xdg_shell.c
@@ -255,10 +255,11 @@ static void xdg_view_handle_commit(struct wl_listener *listener, void *data)
     uint32_t current_serial = xdg_view->wlr_xdg_surface->current.configure_serial;
     /* pending configure has not been acked yet, fix wobbling when resize */
     if (pending_serial >= current_serial && pending_action & VIEW_ACTION_RESIZE) {
-        if (current->x != pending->x) {
+        uint32_t resize_edges = xdg_view->view.current_resize_edges;
+        if (resize_edges & KYWC_EDGE_LEFT) {
             x += pending->width - current->width;
         }
-        if (current->y != pending->y) {
+        if (resize_edges & KYWC_EDGE_TOP) {
             y += pending->height - current->height;
         }
         view_helper_move(view, x, y);

src/xwayland/view.c
@@ -281,6 +281,7 @@ static void xwayland_view_update_geometry(struct xwayland_view *xwayland_view)
 static void xwayland_view_handle_commit(struct wl_listener *listener, void *data)
 {
     struct xwayland_view *xwayland_view = wl_container_of(listener, xwayland_view, commit);
+    uint32_t resize_edges = xwayland_view->view.current_resize_edges;
 
     xwayland_view_update_geometry(xwayland_view);
 
@@ -301,11 +302,11 @@ static void xwayland_view_handle_commit(struct wl_listener *listener, void *data
 
     int x = pending->x, y = pending->y;
     if (pending_action & VIEW_ACTION_RESIZE) {
-        if (current->x != pending->x) {
-            x = pending->x + pending->width - current->width;
+        if (resize_edges & KYWC_EDGE_LEFT) {
+            x += pending->width - current->width;
         }
-        if (current->y != pending->y) {
-            y = pending->y + pending->height - current->height;
+        if (resize_edges & KYWC_EDGE_TOP) {
+            y += pending->height - current->height;
         }
     }
 
@@ -781,16 +782,21 @@ static void xwayland_view_handle_request_configure(struct wl_listener *listener,
 {
     struct xwayland_view *xwayland_view =
         wl_container_of(listener, xwayland_view, request_configure);
-    struct wlr_xwayland_surface_configure_event *event = data;
     struct kywc_view *kywc_view = &xwayland_view->view.base;
-    struct wlr_xwayland_surface *wlr_xwayland_surface = xwayland_view->wlr_xwayland_surface;
 
+    /* skip configure when resizing by left or top edges */
+    if (kywc_view->mapped &&
+        xwayland_view->view.current_resize_edges & (KYWC_EDGE_LEFT | KYWC_EDGE_TOP)) {
+        return;
+    }
+
+    struct wlr_xwayland_surface_configure_event *event = data;
     struct kywc_box geo = { xwayland_unscale(event->x), xwayland_unscale(event->y),
                             xwayland_unscale(event->width), xwayland_unscale(event->height) };
 
     if (!kywc_view->mapped) {
-        wlr_xwayland_surface_configure(wlr_xwayland_surface, event->x, event->y, event->width,
-                                       event->height);
+        wlr_xwayland_surface_configure(xwayland_view->wlr_xwayland_surface, event->x, event->y,
+                                       event->width, event->height);
     } else {
         xwayland_view_adjust_geometry(xwayland_view, &geo);
     }

commit de6746bf32f1e6edb0fae94f8a9b3ec2759b6b11

interactive: use mapped instead of setting to NULL
Fixes: 687ece8c7c3e ("view: add current_resize_edges flag")

Signed-off-by: zhoulei <zhoulei@kylinos.cn>

src/view/interactive.c
@@ -220,9 +220,7 @@ static void interactive_move_show_snap_box(struct interactive_grab *grab, int cu
 
 static void interactive_grab_destroy(struct interactive_grab *grab)
 {
-    if (grab->view) {
-        grab->view->current_resize_edges = KYWC_EDGE_NONE;
-    }
+    grab->view->current_resize_edges = KYWC_EDGE_NONE;
     wl_list_remove(&grab->view_unmap.link);
     seat_end_pointer_grab(grab->seat, &grab->pointer_grab);
     seat_end_keyboard_grab(grab->seat, &grab->keyboard_grab);
@@ -253,7 +251,7 @@ static void interactivate_done_move(struct interactive_grab *grab)
 static void interactive_done(struct interactive_grab *grab)
 {
     /* for snap to edge */
-    if (grab->view && grab->ongoing && grab->mode == INTERACTIVE_MODE_MOVE) {
+    if (grab->view->base.mapped && grab->ongoing && grab->mode == INTERACTIVE_MODE_MOVE) {
         interactivate_done_move(grab);
     }
 
@@ -535,7 +533,6 @@ static const struct seat_touch_grab_interface touch_grab_impl = {
 static void handle_view_unmap(struct wl_listener *listener, void *data)
 {
     struct interactive_grab *grab = wl_container_of(listener, grab, view_unmap);
-    grab->view = NULL;
     interactive_done(grab);
 }
 