e079fc790e2f5cb30438b96c3af13a7c5b4b6566
zhoulei <zhoulei@kylinos.cn>
2024-03-06 17:59:31 +0800

    libkywc: remove provider dir
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- clients/libkywc/libkywc.c
+++ clients/libkywc/libkywc.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 #include "libkywc_p.h"
-#include "provider/provider.h"
+#include "provider.h"
 
 static void registry_handle_global(void *data, struct wl_registry *registry, uint32_t name,
                                    const char *interface, uint32_t version)
