a3cbdc6336c258be2a7cb9942229cc4acb075048
chenyongxing <chenyongxing@kylinos.cn>
2024-05-14 15:03:44 +0800

    effect/touchclick: only gl_render create effect

--- src/effect/touchclick.c
+++ src/effect/touchclick.c
@@ -323,6 +323,10 @@ static const struct effect_interface effect_impl = {
 
 bool touchclick_effect_create(struct effect_manager *manager)
 {
+    if (!wlr_renderer_is_opengl(manager->server->renderer)) {
+        return false;
+    }
+    
     struct touchclick_effect *effect = calloc(1, sizeof(*effect));
     if (!effect) {
         return false;
