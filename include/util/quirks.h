a4371c5342fb672a0a31862a1f31530a97530571
sunzhguy <sunzhigang1@kylinos.cn>
2024-05-21 09:23:54 +0800

    util: add graphics card quirks support
    Closes: #411
    
    Signed-off-by: sunzhguy <sunzhigang1@kylinos.cn>

---
+++ include/util/quirks.h
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2024 KylinSoft Co., Ltd.
+//
+// SPDX-License-Identifier: MulanPSL-2.0
+
+#ifndef _UTIL_QUIRKS_H_
+#define _UTIL_QUIRKS_H_
+
+#include <stdint.h>
+
+enum quirks_mask {
+    QUIRKS_MASK_MASTER_FD = 1 << 0,
+    QUIRKS_MASK_SOFTWARE_CURSOR = 1 << 1,
+    QUIRKS_MASK_EXPLICIT_SYNC = 1 << 2,
+    QUIRKS_MASK_NO_MODIFIFIERS = 1 << 3,
+};
+
+uint32_t quirks_by_backend(int drm_fd);
+
+uint32_t quirks_by_renderer(int drm_fd, const char *vendor_name);
+
+#endif /* _UTIL_QUIRKS_H */
---
+++ src/util/quirks.c
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: 2024 KylinSoft Co., Ltd.
+//
+// SPDX-License-Identifier: MulanPSL-2.0
+
+#include <string.h>
+#include <xf86drm.h>
+
+#include "util/quirks.h"
+
+/* graphics quirks */
+struct quirks {
+    const char *name;   /* dc name */
+    const char *vendor; /* gpu egl-vendor */
+    uint32_t drm_mask;
+    uint32_t render_mask;
+};
+
+static const struct quirks quirks_table[] = {
+    {
+        "hisi",
+        "ARM",
+        .render_mask = QUIRKS_MASK_MASTER_FD,
+    },
+    {
+        "nvidia-drm",
+        "NVIDIA",
+        .drm_mask = QUIRKS_MASK_SOFTWARE_CURSOR,
+        .render_mask = QUIRKS_MASK_EXPLICIT_SYNC,
+    },
+    {
+        "nouveau",
+        NULL,
+        .drm_mask = QUIRKS_MASK_SOFTWARE_CURSOR,
+    },
+    {
+        "vmwgfx",
+        NULL,
+        .drm_mask = QUIRKS_MASK_SOFTWARE_CURSOR,
+    },
+    {
+        "mtgpu",
+        "MTT Mesa Client",
+        .render_mask = QUIRKS_MASK_NO_MODIFIFIERS,
+    },
+    {
+        "mwv207",
+        "Mesa Project",
+        .render_mask = QUIRKS_MASK_NO_MODIFIFIERS,
+    },
+};
+
+uint32_t quirks_by_backend(int drm_fd)
+{
+    drmVersion *version = drmGetVersion(drm_fd);
+    if (!version) {
+        return 0;
+    }
+
+    const struct quirks *quirks = NULL;
+    for (size_t i = 0; i < sizeof(quirks_table) / sizeof(quirks_table[0]); ++i) {
+        if (!quirks_table[i].name || strcmp(quirks_table[i].name, version->name)) {
+            continue;
+        }
+        quirks = &quirks_table[i];
+        break;
+    }
+
+    drmFreeVersion(version);
+
+    return quirks ? quirks->drm_mask : 0;
+}
+
+uint32_t quirks_by_renderer(int drm_fd, const char *vendor_name)
+{
+    drmVersion *version = drmGetVersion(drm_fd);
+    if (!version) {
+        return 0;
+    }
+
+    const struct quirks *quirks = NULL;
+    for (size_t i = 0; i < sizeof(quirks_table) / sizeof(quirks_table[0]); ++i) {
+        if (!quirks_table[i].vendor || strcmp(vendor_name, quirks_table[i].vendor)) {
+            continue;
+        }
+
+        if (quirks_table[i].name && strcmp(quirks_table[i].name, version->name)) {
+            continue;
+        }
+
+        quirks = &quirks_table[i];
+        break;
+    }
+
+    drmFreeVersion(version);
+
+    return quirks ? quirks->render_mask : 0;
+}
