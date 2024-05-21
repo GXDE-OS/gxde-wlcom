53c906a5a2e733498e233f9318cd7edc97f9dd1f
xurui <xurui@kylinos.cn>
2024-05-21 11:29:30 +0800

    view: add views dbus
    Signed-off-by: xurui <xurui@kylinos.cn>

--- src/view/config.c
+++ src/view/config.c
@@ -6,9 +6,118 @@
 #include "view/workspace.h"
 #include "view_p.h"
 
+static const char *service_path = "/com/kylin/Wlcom/Views";
+static const char *service_interface = "com.kylin.Wlcom.Views";
+
+static int get_view_adsorption(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
+{
+    struct view_manager *manager = userdata;
+    return sd_bus_reply_method_return(m, "u", manager->state.view_adsorption);
+}
+
+static int set_view_adsorption(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
+{
+    struct view_manager *manager = userdata;
+    uint32_t adsorption;
+    CK(sd_bus_message_read(m, "u", &adsorption));
+
+    if (adsorption > VIEW_ADSORPTION_ALL) {
+        const sd_bus_error error =
+            SD_BUS_ERROR_MAKE_CONST(SD_BUS_ERROR_INVALID_ARGS, "Invaild adsorption.");
+        return sd_bus_reply_method_error(m, &error);
+    }
+
+    manager->state.view_adsorption = adsorption;
+
+    return sd_bus_reply_method_return(m, NULL);
+}
+
+static int list_all_views(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
+{
+    struct view_manager *manager = userdata;
+    sd_bus_message *reply = NULL;
+    CK(sd_bus_message_new_method_return(m, &reply));
+    CK(sd_bus_message_open_container(reply, 'a', "(ssi)"));
+
+    struct view *view;
+    wl_list_for_each(view, &manager->views, link) {
+        if (!view->base.mapped) {
+            continue;
+        }
+        const char *app_id = view->base.app_id;
+        const char *uuid = view->base.uuid;
+        int pid = view->pid;
+        sd_bus_message_append(reply, "(ssi)", app_id, uuid, pid);
+    }
+
+    CK(sd_bus_message_close_container(reply));
+    CK(sd_bus_send(NULL, reply, NULL));
+    sd_bus_message_unref(reply);
+    return 1;
+}
+
+static int list_view_states(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
+{
+    const char *uuid;
+    CK(sd_bus_message_read(m, "s", &uuid));
+
+    struct kywc_view *kywc_view = kywc_view_by_uuid(uuid);
+    if (!kywc_view) {
+        const sd_bus_error error =
+            SD_BUS_ERROR_MAKE_CONST(SD_BUS_ERROR_INVALID_ARGS, "Invaild uuid.");
+        return sd_bus_reply_method_error(m, &error);
+    }
+
+    sd_bus_message *reply = NULL;
+    CK(sd_bus_message_new_method_return(m, &reply));
+    CK(sd_bus_message_open_container(reply, 'a', "(sai)"));
+
+    sd_bus_message_append(reply, "(sai)", "role", 1, kywc_view->role);
+    sd_bus_message_append(reply, "(sai)", "geometry", 4, kywc_view->geometry.x,
+                          kywc_view->geometry.y, kywc_view->geometry.width,
+                          kywc_view->geometry.height);
+    sd_bus_message_append(reply, "(sai)", "margin", 4, kywc_view->margin.off_x,
+                          kywc_view->margin.off_y, kywc_view->margin.off_width,
+                          kywc_view->margin.off_height);
+    sd_bus_message_append(reply, "(sai)", "padding", 4, kywc_view->padding.top,
+                          kywc_view->padding.bottom, kywc_view->padding.left,
+                          kywc_view->padding.right);
+    sd_bus_message_append(reply, "(sai)", "ssd", 1, kywc_view->ssd);
+    sd_bus_message_append(reply, "(sai)", "minimize size", 2, kywc_view->min_width,
+                          kywc_view->min_height);
+    sd_bus_message_append(reply, "(sai)", "maximize size", 2, kywc_view->max_width,
+                          kywc_view->max_height);
+    sd_bus_message_append(reply, "(sai)", "kept_above", 1, kywc_view->kept_above);
+    sd_bus_message_append(reply, "(sai)", "kept_below", 1, kywc_view->kept_below);
+    sd_bus_message_append(reply, "(sai)", "minimized", 1, kywc_view->minimized);
+    sd_bus_message_append(reply, "(sai)", "maximized", 1, kywc_view->maximized);
+    sd_bus_message_append(reply, "(sai)", "fullscreen", 1, kywc_view->fullscreen);
+    sd_bus_message_append(reply, "(sai)", "activated", 1, kywc_view->activated);
+    sd_bus_message_append(reply, "(sai)", "tiled", 1, kywc_view->tiled);
+    sd_bus_message_append(reply, "(sai)", "modal", 1, kywc_view->modal);
+    sd_bus_message_append(reply, "(sai)", "skip_taskbar", 1, kywc_view->skip_taskbar);
+    sd_bus_message_append(reply, "(sai)", "skip_switcher", 1, kywc_view->skip_switcher);
+
+    CK(sd_bus_message_close_container(reply));
+    CK(sd_bus_send(NULL, reply, NULL));
+    sd_bus_message_unref(reply);
+    return 1;
+}
+
+static const sd_bus_vtable service_vtable[] = {
+    SD_BUS_VTABLE_START(0),
+    SD_BUS_METHOD("GetViewAdsorption", "", "u", get_view_adsorption, 0),
+    SD_BUS_METHOD("SetViewAdsorption", "u", "", set_view_adsorption, 0),
+
+    SD_BUS_METHOD("ListAllViews", "", "a(ssi)", list_all_views, 0),
+    SD_BUS_METHOD("ListViewStates", "s", "a(sai)", list_view_states, 0),
+    SD_BUS_VTABLE_END,
+};
+
 bool view_manager_config_init(struct view_manager *view_manager)
 {
-    view_manager->config = config_manager_add_config("Views", NULL, NULL, NULL, NULL, view_manager);
+    view_manager->config = config_manager_add_config("Views", NULL, service_path, service_interface,
+                                                     service_vtable, view_manager);
     return !!view_manager->config;
 }
 
@@ -23,6 +132,10 @@ bool view_read_config(struct view_manager *view_manager)
         view_manager->state.num_workspaces = json_object_get_int(data);
     }
 
+    if (json_object_object_get_ex(view_manager->config->json, "view_adsorption", &data)) {
+        view_manager->state.view_adsorption = json_object_get_int(data);
+    }
+
     return true;
 }
 
@@ -34,4 +147,7 @@ void view_write_config(struct view_manager *view_manager)
 
     json_object_object_add(view_manager->config->json, "num_workspaces",
                            json_object_new_int(workspace_manager_get_count()));
+
+    json_object_object_add(view_manager->config->json, "view_adsorption",
+                           json_object_new_int(view_manager->state.view_adsorption));
 }
