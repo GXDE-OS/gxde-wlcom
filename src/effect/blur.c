c159ea4683138df22984dc75b2a8d40dd573c92b
chenyongxing <chenyongxing@kylinos.cn>
2024-06-06 11:05:43 +0800

    effect/blur: optimize texture resize.add cache

--- src/effect/blur.c
+++ src/effect/blur.c
@@ -23,14 +23,127 @@
 #include "effect_p.h"
 #include "render/opengl.h"
 #include "scene/scene.h"
+#include "util/time.h"
 
-struct gl_texture {
-    GLenum target;
-    GLuint id;
+// rtt = render to texture = fbo + texture
+struct glrtt_pool_texture {
+    struct wl_list link;
     GLuint fbo;
-    int32_t width;
-    int32_t height;
+    GLuint texture;
+    GLsizei width;
+    GLsizei height;
+    bool used;
+    uint32_t time;
 };
+
+struct glrtt_pool {
+    GLenum format;
+    GLint filter;
+    uint32_t timeout;
+    struct wl_list textures;
+};
+
+static struct glrtt_pool *glrtt_pool_create(GLenum format, GLint filter, uint32_t timeout)
+{
+    struct glrtt_pool *pool = calloc(1, sizeof(*pool));
+    if (!pool) {
+        return NULL;
+    }
+    pool->format = format;
+    pool->filter = filter;
+    pool->timeout = timeout;
+    wl_list_init(&pool->textures);
+    return pool;
+}
+
+static void glrtt_pool_destroy(struct glrtt_pool *pool)
+{
+    struct glrtt_pool_texture *texture, *tmp;
+    wl_list_for_each_safe(texture, tmp, &pool->textures, link) {
+        glDeleteFramebuffers(1, &texture->fbo);
+        glDeleteTextures(1, &texture->texture);
+        wl_list_remove(&texture->link);
+        free(texture);
+    }
+    free(pool);
+}
+
+static struct glrtt_pool_texture *glrtt_pool_get_texture(struct glrtt_pool *pool, GLsizei width,
+                                                         GLsizei height)
+{
+    struct glrtt_pool_texture *texture = NULL;
+
+    // find cached texture
+    bool cached = false;
+    struct glrtt_pool_texture *texture_tmp;
+    wl_list_for_each(texture_tmp, &pool->textures, link) {
+        if (!texture_tmp->used && texture_tmp->width == width && texture_tmp->height == height) {
+            texture = texture_tmp;
+            cached = true;
+            break;
+        }
+    }
+    // new texture
+    if (!cached) {
+        texture = calloc(1, sizeof(*texture));
+        if (!texture) {
+            return NULL;
+        }
+
+        glGenTextures(1, &texture->texture);
+        glBindTexture(GL_TEXTURE_2D, texture->texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, pool->filter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, pool->filter);
+        glTexImage2D(GL_TEXTURE_2D, 0, pool->format, width, height, 0, pool->format,
+                     GL_UNSIGNED_BYTE, 0);
+
+        GLint old_fbo;
+        glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_fbo);
+        glGenFramebuffers(1, &texture->fbo);
+        glBindFramebuffer(GL_FRAMEBUFFER, texture->fbo);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                               texture->texture, 0);
+        GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        if (status != GL_FRAMEBUFFER_COMPLETE) {
+            kywc_log(KYWC_ERROR, "glrtt pool failed to fbo(%d) attach texture(%d). status=%d",
+                     texture->fbo, texture->texture, status);
+        }
+        glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
+
+        texture->width = width;
+        texture->height = height;
+        wl_list_insert(&pool->textures, &texture->link);
+    }
+
+    texture->used = true;
+    return texture;
+}
+
+static void glrtt_pool_release_texture(struct glrtt_pool *pool, struct glrtt_pool_texture *texture)
+{
+    if (!texture) {
+        return;
+    }
+    texture->used = false;
+    texture->time = current_time_msec();
+}
+
+static void glrtt_pool_release_timeout_texture(struct glrtt_pool *pool)
+{
+    struct glrtt_pool_texture *texture, *tmp;
+    wl_list_for_each_safe(texture, tmp, &pool->textures, link) {
+        if (!texture->used && (current_time_msec() - texture->time) > pool->timeout) {
+            glDeleteFramebuffers(1, &texture->fbo);
+            glDeleteTextures(1, &texture->texture);
+            wl_list_remove(&texture->link);
+            free(texture);
+        }
+    }
+}
+
+static struct glrtt_pool *glrtt_pool = NULL;
 
 struct blur_tex_program {
     GLuint id;
@@ -76,11 +189,7 @@ static struct blur_data {
     struct blur_output_data *current_output_data;
 
     float offset;
-
-    struct gl_texture *tex[2];
     struct blur_program blur_prog[2];
-
-    struct gl_texture *blur_tex;
     struct blur_tex_program blur_tex_prog;
 } effect_blur_data = { 0 };
 
@@ -115,93 +224,17 @@ static int calculate_blur_radius(int iterations, float offset)
     return pow(2, iterations + 1) * offset;
 }
 
-static void gl_texture_destroy(struct gl_texture *tex)
-{
-    glDeleteFramebuffers(1, &tex->fbo);
-    glDeleteTextures(1, &tex->id);
-    free(tex);
-}
-
-static struct gl_texture *gl_texture_create(int width, int height)
-{
-    struct gl_texture *tex = calloc(1, sizeof(*tex));
-    if (!tex) {
-        return NULL;
-    }
-
-    GLint old_fbo;
-    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_fbo);
-
-    glGenTextures(1, &tex->id);
-    glBindTexture(GL_TEXTURE_2D, tex->id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-
-    tex->target = GL_TEXTURE_2D;
-    tex->width = width;
-    tex->height = height;
-
-    glGenFramebuffers(1, &tex->fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, tex->fbo);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, tex->target, tex->id, 0);
-
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (status != GL_FRAMEBUFFER_COMPLETE) {
-        kywc_log(KYWC_ERROR, " Failed to create gl texture.");
-        glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
-        glDeleteTextures(1, &tex->id);
-        free(tex);
-        return NULL;
-    }
-
-    glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    return tex;
-}
-
-static void gl_texture_update(struct gl_texture *tex, int width, int height)
-{
-    if (tex->width == width && tex->height == height) {
-        return;
-    }
-    if (tex->id <= 0) {
-        glGenTextures(1, &tex->id);
-    }
-    glBindTexture(GL_TEXTURE_2D, tex->id);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-
-    tex->target = GL_TEXTURE_2D;
-    tex->width = width;
-    tex->height = height;
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-static void gl_texture_copy(struct gl_texture *tex, struct ky_opengl_buffer *src,
+static void gl_texture_copy(struct glrtt_pool_texture *tex, struct ky_opengl_buffer *src,
                             struct kywc_box *box)
 {
-    GLint old_fbo;
-    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_fbo);
-
     glBindFramebuffer(GL_READ_FRAMEBUFFER, src->fbo);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, tex->fbo);
-    glBindTexture(GL_TEXTURE_2D, tex->id);
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex->id, 0);
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (status != GL_FRAMEBUFFER_COMPLETE) {
-        kywc_log(KYWC_ERROR, " Failed to copy fbo %d to texture %d.", src->fbo, tex->id);
-        return;
-    }
     glBlitFramebuffer(box->x, box->y, box->x + box->width, box->y + box->height, 0, 0, box->width,
                       box->height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
     glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
     glBindTexture(GL_TEXTURE_2D, 0);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-
-    glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
 }
 
 #if 0
@@ -312,13 +345,6 @@ static struct blur_tex_program *get_or_generate_blur_text_program(void)
 
 static void blur_data_destroy(struct blur_data *data)
 {
-    if (data->tex[0]) {
-        gl_texture_destroy(data->tex[0]);
-    }
-    if (data->tex[1]) {
-        gl_texture_destroy(data->tex[1]);
-    }
-
     if (data->blur_prog[0].id > 0) {
         glDeleteProgram(data->blur_prog[0].id);
     }
@@ -367,98 +393,96 @@ static void set_tex_matrix(GLint loc, enum wl_output_transform trans, const stru
     glUniformMatrix3fv(loc, 1, GL_FALSE, tex_matrix);
 }
 
-static void render_iteration(struct gl_texture *in, struct gl_texture *out, int width, int height)
+static void render_iteration(struct glrtt_pool_texture *in, struct glrtt_pool_texture *out,
+                             int width, int height)
 {
     /* Special case for small regions where we can't really blur, because we */
     /* simply have too few pixels */
     width = width < 1 ? 1 : width;
     height = height < 1 ? 1 : height;
 
-    gl_texture_update(out, width, height);
-
     glBindFramebuffer(GL_FRAMEBUFFER, out->fbo);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, out->target, out->id, 0);
-
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (status != GL_FRAMEBUFFER_COMPLETE) {
-        kywc_log(KYWC_ERROR, " Failed to create frame buffer.");
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        return;
-    }
     glViewport(0, 0, out->width, out->height);
 
-    glClearColor(1.0f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBindTexture(GL_TEXTURE_2D, in->id);
+    glBindTexture(GL_TEXTURE_2D, in->texture);
     glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
     glBindTexture(GL_TEXTURE_2D, 0);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-static void blur_fb0(struct blur_data *data)
+static void blur_fb0(struct blur_data *data, struct glrtt_pool_texture *src_tex)
 {
+    // iterations must > 0
     int iterations = blur_config.iterations;
     float offset = data->offset;
-
-    int width = data->tex[0]->width;
-    int height = data->tex[0]->height;
+    int width = src_tex->width;
+    int height = src_tex->height;
 
     GLfloat pos_vertex[8] = { -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f };
-    GLint old_fbo;
     GLint viewport[4];
-    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_fbo);
     glGetIntegerv(GL_VIEWPORT, viewport);
 
-    struct gl_texture **tex = data->tex;
     struct blur_program *prog = data->blur_prog;
     struct blur_program *down_prog = &prog[0];
     struct blur_program *up_prog = &prog[1];
 
     glDisable(GL_BLEND);
-
+    // down scale
     glUseProgram(down_prog->id);
     glEnableVertexAttribArray(down_prog->shaders.position);
     glVertexAttribPointer(down_prog->shaders.position, 2, GL_FLOAT, GL_FALSE, 0, pos_vertex);
     glUniform1f(down_prog->shaders.offset, offset);
 
-    int sampleWidth, sampleHeight;
-    pixman_region32_t region;
-    pixman_region32_init(&region);
+    struct glrtt_pool_texture *last_tex = src_tex;
     for (int i = 0; i < iterations; i++) {
-        sampleWidth = width / (1 << i);
-        sampleHeight = height / (1 << i);
+        int sample_width = width / (1 << (i + 1));
+        int sample_height = height / (1 << (i + 1));
 
-        glUniform2f(down_prog->shaders.halfpixel, 0.5f / sampleWidth, 0.5f / sampleHeight);
-        render_iteration(tex[i % 2], tex[1 - i % 2], sampleWidth, sampleHeight);
-        data->blur_tex = tex[1 - i % 2];
+        // first iter input use src_tex
+        struct glrtt_pool_texture *in_tex = last_tex;
+        struct glrtt_pool_texture *out_tex =
+            glrtt_pool_get_texture(glrtt_pool, sample_width, sample_height);
+
+        glUniform2f(down_prog->shaders.halfpixel, 0.5f / sample_width, 0.5f / sample_height);
+        render_iteration(in_tex, out_tex, sample_width, sample_height);
+
+        if (i != 0) {
+            glrtt_pool_release_texture(glrtt_pool, in_tex);
+        }
+        last_tex = out_tex;
     }
     glDisableVertexAttribArray(down_prog->shaders.position);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glUseProgram(0);
 
+    // up scale
     glUseProgram(up_prog->id);
     glEnableVertexAttribArray(up_prog->shaders.position);
     glVertexAttribPointer(up_prog->shaders.position, 2, GL_FLOAT, GL_FALSE, 0, pos_vertex);
     glUniform1f(up_prog->shaders.offset, offset);
     for (int i = iterations - 1; i >= 0; i--) {
-        sampleWidth = width / (1 << i);
-        sampleHeight = height / (1 << i);
+        int sample_width = width / (1 << i);
+        int sample_height = height / (1 << i);
 
-        glUniform2f(up_prog->shaders.halfpixel, 0.5f / sampleWidth, 0.5f / sampleHeight);
-        render_iteration(tex[1 - i % 2], tex[i % 2], sampleWidth, sampleHeight);
-        data->blur_tex = tex[i % 2];
+        // last iter output use src_tex
+        struct glrtt_pool_texture *in_tex = last_tex;
+        struct glrtt_pool_texture *out_tex =
+            i == 0 ? src_tex : glrtt_pool_get_texture(glrtt_pool, sample_width, sample_height);
+
+        glUniform2f(up_prog->shaders.halfpixel, 0.5f / sample_width, 0.5f / sample_height);
+        render_iteration(in_tex, out_tex, sample_width, sample_height);
+
+        glrtt_pool_release_texture(glrtt_pool, in_tex);
+        last_tex = out_tex;
     }
     glDisableVertexAttribArray(up_prog->shaders.position);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glUseProgram(0);
-    pixman_region32_fini(&region);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     glBindTexture(GL_TEXTURE_2D, 0);
-    glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
     glViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
 }
 
@@ -523,6 +547,9 @@ static void blur_render(struct ky_scene_render_target *target,
                         const struct blur_render_options *options,
                         const pixman_region32_t *blur_region)
 {
+    GLint old_fbo;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_fbo);
+
     struct ky_opengl_render_pass *gl_pass =
         ky_opengl_render_pass_from_wlr_render_pass(target->render_pass);
 
@@ -534,28 +561,22 @@ static void blur_render(struct ky_scene_render_target *target,
         .width = cpy_box->x2 - cpy_box->x1,
         .height = cpy_box->y2 - cpy_box->y1,
     };
+    struct glrtt_pool_texture *src_tex =
+        glrtt_pool_get_texture(glrtt_pool, buffer_cpy_box.width, buffer_cpy_box.height);
+    if (!src_tex) {
+        return;
+    }
+    gl_texture_copy(src_tex, gl_pass->buffer, &buffer_cpy_box);
+
     struct blur_data *data = &effect_blur_data;
-    data->tex[0] = data->tex[0] ? data->tex[0]
-                                : gl_texture_create(buffer_cpy_box.width, buffer_cpy_box.height);
-    if (!data->tex[0]) {
-        return;
-    }
-    gl_texture_update(data->tex[0], buffer_cpy_box.width, buffer_cpy_box.height);
-    gl_texture_copy(data->tex[0], gl_pass->buffer, &buffer_cpy_box);
-
-    data->tex[1] = data->tex[1] ? data->tex[1]
-                                : gl_texture_create(buffer_cpy_box.width, buffer_cpy_box.height);
-    if (!data->tex[1]) {
-        return;
-    }
-
-    data->blur_tex = NULL;
     if ((int)options->strength == -1) {
         data->offset = 4.f;
     } else {
         data->offset = options->strength / 1000.f;
     }
-    blur_fb0(data);
+
+    blur_fb0(data, src_tex);
+    struct glrtt_pool_texture *blurred_tex = src_tex;
 
     struct blur_tex_program *prog = &data->blur_tex_prog;
     if (!prog) {
@@ -564,15 +585,14 @@ static void blur_render(struct ky_scene_render_target *target,
     glUseProgram(prog->id);
 
     glActiveTexture(GL_TEXTURE0);
-    struct gl_texture *texture = data->blur_tex;
-    glBindTexture(texture->target, texture->id);
+    glBindTexture(GL_TEXTURE_2D, blurred_tex->texture);
 
     if (target->scale - floor(target->scale) > 0.001) {
-        glTexParameteri(texture->target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(texture->target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     } else {
-        glTexParameteri(texture->target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(texture->target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     }
 
     struct wlr_fbox src_fbox = { .x = 0, .y = 0, .width = 1.0f, .height = 1.0f };
@@ -597,10 +617,16 @@ static void blur_render(struct ky_scene_render_target *target,
                 options->radius->rt / half_height, options->radius->lb / half_height,
                 options->radius->lt / half_height);
 
+    glBindFramebuffer(GL_FRAMEBUFFER, old_fbo);
     render(&buffer_cpy_box, blur_region, prog->shaders.pos_attrib, options->dst_box,
            prog->shaders.sdfpos_attrib);
 
-    glBindTexture(texture->target, 0);
+    glrtt_pool_release_texture(glrtt_pool, src_tex);
+    // call release_timeout in blur, better than frame_pre()
+    // in frame_pre will pre release blur need use texture
+    glrtt_pool_release_timeout_texture(glrtt_pool);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
     glUseProgram(0);
 }
 
@@ -691,6 +717,7 @@ static bool blur_frame_render_pre(struct effect_entity *entity,
         }
     }
     data->current_output_data = output_data;
+
     return true;
 }
 
@@ -847,6 +874,8 @@ static void handle_effect_disable(struct wl_listener *listener, void *data)
 
 static void handle_effect_destroy(struct wl_listener *listener, void *data)
 {
+    glrtt_pool_destroy(glrtt_pool);
+
     struct blur_effect *effect = wl_container_of(listener, effect, destroy);
     wl_list_remove(&effect->destroy.link);
     wl_list_remove(&effect->enable.link);
@@ -901,6 +930,8 @@ bool blur_effect_create(struct effect_manager *effect_manager)
         return false;
     }
     entity->user_data = &effect_blur_data;
+
+    glrtt_pool = glrtt_pool_create(GL_RGBA, GL_LINEAR, 3000);
 
     return true;
 }
