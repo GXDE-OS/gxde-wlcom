c7b6e60cd0c8885df8dfdebf34d333b9cba0ee03
yizixiao <yizixiao@kylinos.cn>
2024-06-17 15:38:29 +0800

    workspace: change the initial number of workspaces to 4
    BUG 232049
    
    Signed-off-by: yizixiao <yizixiao@kylinos.cn>

--- src/view/view.c
+++ src/view/view.c
@@ -1562,7 +1562,7 @@ struct view_manager *view_manager_create(struct server *server)
 
     view_manager_config_init(view_manager);
 
-    view_manager->state.num_workspaces = 1;
+    view_manager->state.num_workspaces = 4;
     view_manager->state.view_adsorption = VIEW_ADSORPTION_ALL;
     view_read_config(view_manager);
 
