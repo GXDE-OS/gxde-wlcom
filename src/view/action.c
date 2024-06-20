8e2b4390228054a46133b382ad7762918a764b47
zhoulei <zhoulei@kylinos.cn>
2024-06-20 14:39:57 +0800

    action: don't show menu when fullscreen
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/view/action.c
+++ src/view/action.c
@@ -190,9 +190,9 @@ void window_action(struct view *view, struct seat *seat, enum window_action acti
         kywc_view_toggle_kept_below(kywc_view);
         break;
     case WINDOW_ACTION_MENU:
-        if (kywc_view->ssd & KYWC_SSD_TITLE) {
-            lx = kywc_view->geometry.x - kywc_view->margin.off_x / 2;
-            ly = kywc_view->geometry.y - kywc_view->margin.off_y / 2;
+        if (kywc_view->ssd & KYWC_SSD_TITLE && !kywc_view->fullscreen) {
+            lx = kywc_view->geometry.x - 8;
+            ly = kywc_view->geometry.y;
             view_show_window_menu(view, seat, lx, ly);
         }
         break;
