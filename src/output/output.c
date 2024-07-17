16b4b8ce3f8108b1db7ced8a050bef25878028b8
sunzhguy <sunzhigang1@kylinos.cn>
2024-07-17 14:25:48 +0800

    output: fix output config
    Reject auto-coordinate cannot be configured to zero
    
    Signed-off-by: sunzhguy <sunzhigang1@kylinos.cn>

--- src/output/output.c
+++ src/output/output.c
@@ -724,7 +724,7 @@ bool output_manager_configure_outputs(void)
         have_zero_coord = true;
     }
 
-    if (!have_enabled_output || (!have_zero_coord && !have_auto_coord)) {
+    if (!have_enabled_output || !have_zero_coord) {
         kywc_log(KYWC_WARN, "All outputs will be disabled or no zero coord, reject this configure");
         goto failed;
     }
