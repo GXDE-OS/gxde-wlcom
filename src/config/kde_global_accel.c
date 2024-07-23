caf20b8cf8e48c5892ff6fb0a638ac56f6fc5c2a
zhoulei <zhoulei@kylinos.cn>
2024-07-23 13:43:32 +0800

    config/qt-key: remove duplicate keys
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/config/kde_global_accel.c
+++ src/config/kde_global_accel.c
@@ -93,7 +93,7 @@ static uint32_t qtkey_to_keysym(int32_t key)
     }
 
     if (sym == XKB_KEY_NoSymbol) {
-        kywc_log(KYWC_WARN, "cannot covert qtkey %d to xkb symbol", qtKey);
+        kywc_log(KYWC_WARN, "cannot covert qtkey 0x%8x to xkb symbol", key);
     }
 
     return sym;
