5ffbaa285706c441ef36350b4b35aa117ee1db1c
yiqiang <yiqiang@kylinos.cn>
2024-08-14 11:29:59 +0800

    scene: use the extended damage rendering software cursor
    Signed-off-by: yiqiang <yiqiang@kylinos.cn>

--- src/scene/render.c
+++ src/scene/render.c
@@ -40,9 +40,6 @@ void ky_scene_render_damage_in_target(struct ky_scene *scene, struct ky_scene_re
         return;
     }
 
-    pixman_region32_t damage;
-    pixman_region32_init(&damage);
-    pixman_region32_copy(&damage, &target->damage);
     // to scene layout coord
     pixman_region32_translate(&target->damage, target->logical.x, target->logical.y);
 
@@ -71,6 +68,10 @@ void ky_scene_render_damage_in_target(struct ky_scene *scene, struct ky_scene_re
     ky_scene_output_render_end(target);
 
     // for software cursor
+    pixman_region32_t damage;
+    pixman_region32_init(&damage);
+    pixman_region32_copy(&damage, &target->damage);
+    pixman_region32_translate(&damage, -target->logical.x, -target->logical.y);
     wlr_region_scale(&damage, &damage, target->scale);
     wlr_output_add_software_cursors_to_render_pass(target->output->output, target->render_pass,
                                                    &damage);
