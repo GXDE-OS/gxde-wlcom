c3db880d3a4e45a67f9c6b8510673a156a981348
xurui <xurui@kylinos.cn>
2024-08-28 09:49:21 +0800

    effect/scale: set input bypassed if view minimized or not
    Signed-off-by: xurui <xurui@kylinos.cn>

--- src/effect/scale.c
+++ src/effect/scale.c
@@ -516,11 +516,11 @@ bool view_add_scale_effect(struct view *view, enum scale_action action)
         return false;
     }
 
+    ky_scene_node_set_input_bypassed(&view->tree->node, view->base.minimized);
     /* scale effect interrupted */
     if (entity->user_data) {
         if (view->base.minimized) {
             ky_scene_node_set_enabled(&view->tree->node, true);
-            ky_scene_node_set_input_bypassed(&view->tree->node, true);
         }
         struct scale_effect_data *scale_data = entity->user_data;
         scale_data->action = action;
@@ -533,7 +533,6 @@ bool view_add_scale_effect(struct view *view, enum scale_action action)
 
     if (view->base.minimized) {
         ky_scene_node_set_enabled(&view->tree->node, true);
-        ky_scene_node_set_input_bypassed(&view->tree->node, true);
         ky_scene_node_raise_to_top(view->current_proxy ? &view->current_proxy->tree->node
                                                        : &view->tree->node);
     }
