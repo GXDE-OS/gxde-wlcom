88745f6b81efee5b708a161ce40ab05befd58953
zhoulei <zhoulei@kylinos.cn>
2024-09-30 15:43:09 +0800

    config: add trim memory dbus
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/config/common.c
+++ src/config/common.c
@@ -2,11 +2,19 @@
 //
 // SPDX-License-Identifier: GPL-1.0-or-later
 
+#include <malloc.h>
+
 #include "config_p.h"
+#include "server.h"
 #include "util/logger.h"
 
 static const char *service_path = "/com/kylin/Wlcom";
 static const char *service_interface = "com.kylin.Wlcom";
+
+static struct {
+    struct wl_listener destroy;
+    struct wl_event_source *timer;
+} config = { 0 };
 
 static int set_log_level(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
 {
@@ -36,15 +44,59 @@ static int print_config(sd_bus_message *m, void *userdata, sd_bus_error *ret_err
     return 1;
 }
 
+static int handle_timeout(void *data)
+{
+    wl_event_source_timer_update(config.timer, 5000);
+    malloc_trim(4096);
+    return 0;
+}
+
+static int trim_memory(sd_bus_message *m, void *userdata, sd_bus_error *ret_error)
+{
+    uint32_t enabled = 0;
+    CK(sd_bus_message_read(m, "b", &enabled));
+
+    if (!!enabled == !!config.timer) {
+        return sd_bus_reply_method_return(m, NULL);
+    }
+
+    struct config_manager *cm = userdata;
+    if (enabled) {
+        config.timer = wl_event_loop_add_timer(cm->server->event_loop, handle_timeout, cm);
+        wl_event_source_timer_update(config.timer, 5000);
+    } else {
+        wl_event_source_remove(config.timer);
+        config.timer = NULL;
+    }
+
+    return sd_bus_reply_method_return(m, NULL);
+}
+
 static const sd_bus_vtable service_vtable[] = {
     SD_BUS_VTABLE_START(0),
     SD_BUS_METHOD("SetLogLevel", "u", "", set_log_level, 0),
     SD_BUS_METHOD("PrintConfig", "", "s", print_config, 0),
+    SD_BUS_METHOD("TrimMemory", "b", "", trim_memory, 0),
     SD_BUS_VTABLE_END,
 };
 
+static void handle_destroy(struct wl_listener *listener, void *data)
+{
+    wl_list_remove(&config.destroy.link);
+    if (config.timer) {
+        wl_event_source_remove(config.timer);
+    }
+}
+
 bool config_manager_common_init(struct config_manager *config_manager)
 {
-    return !!config_manager_add_config(NULL, NULL, service_path, service_interface, service_vtable,
-                                       config_manager);
+    if (!config_manager_add_config(NULL, NULL, service_path, service_interface, service_vtable,
+                                   config_manager)) {
+        return false;
+    }
+
+    config.destroy.notify = handle_destroy;
+    wl_display_add_destroy_listener(config_manager->server->display, &config.destroy);
+
+    return true;
 }
