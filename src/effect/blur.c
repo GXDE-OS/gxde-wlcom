32c65df50733d529a01b98d4b4ce5f2df2a5a812
yiqiang <yiqiang@kylinos.cn>
2024-10-16 16:30:03 +0800

    effect/blur: disable formatting for ALIGN macro definition
    Signed-off-by: yiqiang <yiqiang@kylinos.cn>

--- src/effect/blur.c
+++ src/effect/blur.c
@@ -552,7 +552,9 @@ static void render(const struct kywc_box *box, const pixman_region32_t *blur, GL
     glDisableVertexAttribArray(sdfpos_attrib);
 }
 
+// clang-format off
 #define ALIGN(x, y) (((x) + ((y) - 1)) & ~((y) - 1))
+// clang-format on
 
 static void blur_render(struct ky_scene_render_target *target,
                         const struct blur_render_options *options,
