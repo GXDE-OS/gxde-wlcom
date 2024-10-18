c6c3b636cf4f826ae17f76db8bc024f6d36fdb1e
yuanye <yuanye1@kylinos.cn>
2024-10-18 09:25:04 +0800

    xwayland/view: if the size does not change, move directly
    Signed-off-by: yuanye <yuanye1@kylinos.cn>

--- src/xwayland/view.c
+++ src/xwayland/view.c
@@ -213,7 +213,10 @@ static void xwayland_view_configure(struct view *view)
     /* direct move when not changed size */
     if (view->pending.action & VIEW_ACTION_MOVE) {
         view->pending.action &= ~VIEW_ACTION_MOVE;
-        if (!view_action_change_size(view->pending.configure_action)) {
+        struct kywc_box *current = &view->base.geometry;
+        struct kywc_box *configure = &view->pending.configure_geometry;
+        if (!view_action_change_size(view->pending.action) ||
+            (current->width == configure->width && current->height == configure->height)) {
             xwayland_view_move(xwayland_view, view->pending.geometry.x, view->pending.geometry.y);
         } else {
             kywc_log(KYWC_DEBUG, "skip move when pending configure action 0x%x",
