9e4c994cee9d9202679853007a9099be89836676
yiqiang <yiqiang@kylinos.cn>
2024-10-21 17:16:57 +0800

    renderer/profile: check the validity of the renderer pointer
    Signed-off-by: yiqiang <yiqiang@kylinos.cn>

--- include/render/profile.h
+++ include/render/profile.h
@@ -10,17 +10,17 @@
 #include <wlr/render/wlr_renderer.h>
 
 // clang-format off
-#define KY_PROFILE_RENDER_CREATE(renderer) ky_profile_render_create(renderer)
-#define KY_PROFILE_RENDER_ZONE_COLOR(renderer, var, name, color) do { static const struct ___tracy_source_location_data var = { name, __func__, __FILE__, __LINE__, color }; ky_profile_render_begin(renderer, &var); } while (0)
-#define KY_PROFILE_RENDER_ZONE(renderer, var, name) KY_PROFILE_RENDER_ZONE_COLOR(renderer, var, name, 0x8b0000)
-#define KY_PROFILE_RENDER_ZONE_END(renderer) ky_profile_render_end(renderer)
-#define KY_PROFILE_RENDER_COLLECT(renderer) ky_profile_render_collect(renderer)
+#define KY_PROFILE_RENDER_CREATE(renderer) do { if (renderer) ky_profile_render_create(renderer); } while (0)
+#define KY_PROFILE_RENDER_ZONE_COLOR(renderer, var, name, color) do { static const struct ___tracy_source_location_data var = { name, __func__, __FILE__, __LINE__, color }; if (renderer) ky_profile_render_begin(renderer, &var); } while (0)
+#define KY_PROFILE_RENDER_ZONE(renderer, var, name) do { if (renderer) KY_PROFILE_RENDER_ZONE_COLOR(renderer, var, name, 0x8b0000); } while (0)
+#define KY_PROFILE_RENDER_ZONE_END(renderer) do { if (renderer) ky_profile_render_end(renderer); } while (0)
+#define KY_PROFILE_RENDER_COLLECT(renderer) do { if (renderer) ky_profile_render_collect(renderer); } while (0)
 // clang-format on
 
 void ky_profile_render_create(struct wlr_renderer *renderer);
 void ky_profile_render_destroy(struct wlr_renderer *renderer);
 void ky_profile_render_begin(struct wlr_renderer *renderer,
-                          const struct ___tracy_source_location_data *data);
+                             const struct ___tracy_source_location_data *data);
 void ky_profile_render_end(struct wlr_renderer *renderer);
 void ky_profile_render_collect(struct wlr_renderer *renderer);
 
