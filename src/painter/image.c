9cb6286dccddbf6192799cb19bb02d2f8c4ca31b
zhoulei <zhoulei@kylinos.cn>
2025-01-09 10:33:31 +0800

    painter/image: add bmp and ico format support
    Signed-off-by: zhoulei <zhoulei@kylinos.cn>

--- src/painter/image.c
+++ src/painter/image.c
@@ -211,6 +211,138 @@ static uint8_t *do_decode_png(const uint8_t *data, size_t size, uint32_t *width,
     return buffer;
 }
 
+static bool image_is_png(const uint8_t *data)
+{
+    return data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4E && data[3] == 0x47 &&
+           data[4] == 0x0D && data[5] == 0x0A && data[6] == 0x1A && data[7] == 0x0A;
+}
+
+typedef struct __attribute__((__packed__)) {
+    uint16_t type;
+    uint32_t file_size;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t offset;
+} bmp_file_header;
+
+typedef struct __attribute__((__packed__)) {
+    uint32_t dib_size;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t bpp;
+    uint32_t compression;
+    uint32_t img_size;
+    uint32_t hres;
+    uint32_t vres;
+    uint32_t clr_palette;
+    uint32_t clr_important;
+} bmp_info_header;
+
+static uint8_t *do_decode_bmp(const uint8_t *data, const uint8_t *pixel, uint32_t *width,
+                              uint32_t *height)
+{
+    const bmp_info_header *header = (bmp_info_header *)data;
+    if (header->bpp != 32 || header->compression != 0) {
+        return NULL;
+    }
+
+    uint32_t stride = header->width * 4;
+    // The height of the BMP image must be twice the height in ico
+    uint32_t h = pixel ? abs(header->height) : abs(header->height) / 2;
+
+    uint8_t *buffer = malloc(stride * h);
+    if (!buffer) {
+        return NULL;
+    }
+
+    const uint8_t *src = pixel ? pixel : data + sizeof(*header);
+    // default is bottom-to-top
+    if (header->height < 0) {
+        memcpy(buffer, src, stride * h);
+    } else {
+        for (uint32_t i = 0; i < h; i++) {
+            memcpy(buffer + i * stride, src + (h - 1 - i) * stride, stride);
+        }
+    }
+
+    *width = header->width;
+    *height = h;
+
+    return buffer;
+}
+
+static uint8_t *do_decode_ico(const uint8_t *data, size_t size, uint32_t *width, uint32_t *height)
+{
+    struct __attribute__((__packed__)) ico_header {
+        uint16_t reserved;
+        uint16_t type; // 1 for icon, 2 for cursor
+        uint16_t count;
+    };
+
+    struct __attribute__((__packed__)) ico_directory {
+        uint8_t width;  // should be 0 if 256 pixels
+        uint8_t height; // should be 0 if 256 pixels
+        uint8_t count;  // should be 0 if more than 256 colors
+        uint8_t reserved;
+        uint16_t planes; // should be 0 or 1
+        uint16_t bpp;
+        uint32_t size;
+        uint32_t offset;
+    };
+
+    struct __attribute__((__packed__)) ico {
+        struct ico_header header;
+        struct ico_directory dict[];
+    };
+
+    const struct ico *ico = (struct ico *)data;
+    // header is checked in image_decode_file
+    const struct ico_directory *best_dict = NULL, *max_dict = NULL;
+    // use max size 256 if not set
+    uint32_t best_width = *width ? *width : 256;
+    uint32_t min_width = UINT_MAX, max_width = 0;
+
+    for (uint16_t i = 0; i < ico->header.count; i++) {
+        const struct ico_directory *dict = &ico->dict[i];
+        if (dict->bpp != 32) {
+            continue;
+        }
+        uint32_t width = dict->width ? dict->width : 256;
+        if (width == best_width) {
+            best_dict = dict;
+            break;
+        }
+        if (width > best_width && width < min_width) {
+            min_width = width;
+            best_dict = dict;
+        }
+        if (width > max_width) {
+            max_width = width;
+            max_dict = dict;
+        }
+    }
+
+    if (!best_dict && max_dict) {
+        best_dict = max_dict;
+    }
+    if (!best_dict) {
+        return NULL;
+    }
+
+    const uint8_t *pixels = data + best_dict->offset;
+    uint8_t *buffer = NULL;
+
+    if (image_is_png(pixels)) {
+        buffer = do_decode_png(pixels, best_dict->size, width, height);
+    } else if (*(uint32_t *)pixels == 0x28) {
+        // it must exclude the opening BITMAPFILEHEADER structure
+        buffer = do_decode_bmp(pixels, NULL, width, height);
+    }
+
+    return buffer;
+}
+
 uint8_t *image_decode_file(const char *file, uint32_t *width, uint32_t *height)
 {
     int fd = open(file, O_RDONLY);
@@ -224,7 +356,7 @@ uint8_t *image_decode_file(const char *file, uint32_t *width, uint32_t *height)
         return NULL;
     }
 
-    void *data = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    uint8_t *data = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (data == MAP_FAILED) {
         close(fd);
         return NULL;
@@ -236,14 +368,18 @@ uint8_t *image_decode_file(const char *file, uint32_t *width, uint32_t *height)
         return NULL;
     }
 
-    uint8_t *header = data;
     uint8_t *buffer = NULL;
 
-    if (header[0] == 0x89 && header[1] == 0x50 && header[2] == 0x4E && header[3] == 0x47 &&
-        header[4] == 0x0D && header[5] == 0x0A && header[6] == 0x1A && header[7] == 0x0A) {
+    if (image_is_png(data)) {
         buffer = do_decode_png(data, st.st_size, width, height);
-    } else if (header[0] == 0xFF && header[1] == 0xD8) {
+    } else if (data[0] == 0xFF && data[1] == 0xD8) {
         buffer = do_decode_jpeg(data, st.st_size, width, height);
+    } else if (data[0] == 0x00 && data[1] == 0x00 && data[2] == 0x01 && data[3] == 0x00) {
+        buffer = do_decode_ico(data, st.st_size, width, height);
+    } else if (data[0] == 0x42 && data[1] == 0x4D &&
+               *(uint32_t *)(data + sizeof(bmp_file_header)) == 0x28) {
+        buffer = do_decode_bmp(data + sizeof(bmp_file_header),
+                               data + ((bmp_file_header *)data)->offset, width, height);
     } else {
         kywc_log(KYWC_WARN, "%s: unsupported image format", file);
     }
@@ -267,46 +403,21 @@ bool cairo_buffer_write_to_bmp(struct cairo_buffer *buffer, const char *filename
     int stride = cairo_image_surface_get_stride(buffer->surface);
     uint8_t *data = cairo_image_surface_get_data(buffer->surface);
 
-    struct {
-        unsigned int biSize;
-        int biWidth;
-        int biHeight;
-        unsigned short biPlanes;
-        unsigned short biBitCount;
-        unsigned int biCompression;
-        unsigned int biSizeImage;
-        int biXPelsPerMeter;
-        int biYPelsPerMeter;
-        unsigned int biClrUsed;
-        unsigned int biClrImportant;
-    } info_header = {
-        .biSize = sizeof(info_header),
-        .biWidth = width,
-        .biHeight = -height,
-        .biPlanes = 1,
-        .biBitCount = 32,
-        .biCompression = 0,
-        .biSizeImage = stride * height,
-        .biXPelsPerMeter = 0,
-        .biYPelsPerMeter = 0,
-        .biClrUsed = 0,
-        .biClrImportant = 0,
+    bmp_info_header info_header = {
+        .dib_size = sizeof(info_header),
+        .width = width,
+        .height = -height,
+        .planes = 1,
+        .bpp = 32,
+        .img_size = stride * height,
     };
 
-    struct {
-        unsigned int bfSize;
-        unsigned short bfReserved1;
-        unsigned short bfReserved2;
-        unsigned int bfOffBits;
-    } file_header = {
-        .bfSize = 2 + sizeof(file_header) + sizeof(info_header) + stride * height,
-        .bfReserved1 = 0,
-        .bfReserved2 = 0,
-        .bfOffBits = 2 + sizeof(file_header) + sizeof(info_header),
+    bmp_file_header file_header = {
+        .type = 'B' | ('M' << 8),
+        .file_size = sizeof(file_header) + sizeof(info_header) + info_header.img_size,
+        .offset = sizeof(file_header) + sizeof(info_header),
     };
 
-    unsigned short type = 0x4D42;
-    fwrite(&type, sizeof(type), 1, file);
     fwrite(&file_header, sizeof(file_header), 1, file);
     fwrite(&info_header, sizeof(info_header), 1, file);
     fwrite(data, stride, height, file);
